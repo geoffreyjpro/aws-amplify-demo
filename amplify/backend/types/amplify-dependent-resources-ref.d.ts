d3a4f4a49f9811d2d58a3b9969733b67a86ffdb5
Geoffrey Rekier <geoffrey@agilxp.com>
2023-01-17 13:12:09 +0100

    Add complete example

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsamplifydemo": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "awsamplifydemod8d38f62": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, TextField, withAuthenticator} from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+import {API} from "aws-amplify";
+import * as mutations from "./graphql/mutations";
+import * as queries from "./graphql/queries";
+import {useEffect, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const defaultForm = {
+    name: "",
+    description: ""
+};
+
+function App({signOut, user}) {
+    const [todos, setTodos] = useState([]);
+    const [data, updateData] = useState(defaultForm);
+
+    useEffect(() => {
+        fetchAllToDos();
+    }, [])
+
+    const createToDo = async (todoDetails) => {
+        const newTodo = await API.graphql({query: mutations.createTodo, variables: {input: todoDetails}});
+    }
+
+    const updateToDo = async (todoDetails) => {
+        // N.B. todoDetails must have an ID here!!!
+        const newTodo = await API.graphql({query: mutations.updateTodo, variables: {input: todoDetails}});
+    }
+
+    const deleteToDo = async (todoDetails) => {
+        // N.B. todoDetails must have an ID here!!! (only ID needed)
+        const newTodo = await API.graphql({query: mutations.deleteTodo, variables: {input: todoDetails}});
+    }
+
+    const fetchAllToDos = async () => {
+        const allTodos = await API.graphql({ query: queries.listTodos });
+        setTodos(allTodos.data.listTodos.items)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        createToDo(data).then(() => {
+            updateData(defaultForm)
+            fetchAllToDos()
+        })
+    }
+
+    const handleChange = (e) => {
+        return updateData(values => ({
+            ...values,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    return (
+        <div className="App">
+            <h1>Hello {user.attributes.email}</h1>
+            <button onClick={signOut}>Sign out</button>
+            {todos.map((todo) => (
+                <p key={todo.id}>{todo.name} : {todo.description}</p>
+            ))}
+            <form onSubmit={handleSubmit}>
+                <TextField label={'Task name'} type={'text'} name={'name'} value={data.name} onChange={handleChange} />
+                <TextField label={'Task description'} type={'text'} name={'description'} value={data.description} onChange={handleChange} />
+                <Button type={'submit'} color={'primary'}>Create New Task</Button>
+            </form>
+        </div>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
